3b8c7bf35abc65126b0b5ec685eef78eaf31810c
bernibra <bernat.bramon@gmail.com>
2022-02-11 17:50:42 +0100

    Fixing bug

--- code/get_file.R
+++ code/get_file.R
@@ -4,7 +4,7 @@ get_file <- function(id, dest_dir, download_date = NULL){
   if(!file.exists(file.path(rdir, id))){
     dir.create(rdir, showWarnings = FALSE)
     message("donwloading data on ", download_date)
-    getGEO(id, destdir = rdir)
+    GEOquery::getGEO(id, destdir = rdir)
   } else {
     message("file already found")
     return(0)

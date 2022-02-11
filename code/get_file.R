bfdc18cabe903f34352eefbdd140bbe7b43018ee
bernibra <bernat.bramon@gmail.com>
2022-02-11 14:47:04 +0100

    Add basic function to downlad GEO data

---
+++ code/get_file.R
@@ -0,0 +1,12 @@
+# Download raw data
+get_file <- function(id, dest_dir, download_date = NULL){
+  rdir <- file.path(dest_dir, id)
+  if(!file.exists(file.path(rdir, id))){
+    dir.create(rdir, showWarnings = FALSE)
+    message("donwloading data on ", download_date)
+    getGEO(id, destdir = rdir)
+  } else {
+    message("file already found")
+    return(0)
+  }
+}

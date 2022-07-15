7117526d6582e588ae653151fcadc303bb36e399
Bernat Bramon Mora <bbramonm@login.curnagl>
2022-07-15 13:30:33 +0200

    Adding sbatch for slurm in unil HPC

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account rgottar1_citeseq
+#SBATCH --time 00-12:00:00
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 100G
+#SBATCH -o /scratch/bbramonm/output_%j.txt
+#SBATCH -e /scratch/bbramonm/errors_%j.txt
+
+module load singularity
+
+export SINGULARITY_BINDPATH="/users,/scratch"
+
+singularity run ./R-4.2.1-cite.sif Rscript --vanilla main.R
+
